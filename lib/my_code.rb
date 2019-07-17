8169ebc04b728e21693443af04743ed3cd6a7e68
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-17 15:11:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(array)
+  i = 0
+  new_array = []
+  
+  while i < array.length do 
+    new_array.push(yield(array[i]))
+    i = i + 1 
+  end
+  return new_array 
+end
+
+def reduce(array, memo)
+  value
+  i = 0
+  while i < array.length do
+    if i = 0
+    array[i] + array[i + 1] = value 
+    i = i + 1
+  else
+    
+    
+      
+    
+    
+    
+    
